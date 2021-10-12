915935170ac225fcdc0921842a6b17cd3eb7f491
ksm0207 <ksm03071@naver.com>
2021-10-12 20:26:36 +0900

    dd
    d

--- study/main.js
+++ study/main.js
@@ -4,3 +4,16 @@ console.log("Hello World");
 let a;
 
 a = 6;
+
+
+function test(){
+    let id = document.getElementById('s_id');
+
+    if (id === undefined){
+
+        if(id.le){
+
+        }
+    }
+
+}
